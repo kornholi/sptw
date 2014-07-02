788911c9b249b41f1e6d4cc3be3c833148d52132
Kornelijus Survila <kornholijo@gmail.com>
2014-07-02 14:52:37 -0500

    Combine examples

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 
 #include <ogr_spatialref.h>
 
@@ -8,7 +9,8 @@
 int main(int argc, char *argv[])
 {
     const int TILE_SIZE = 16;
-    const int RASTER_SIZE = 8192;
+    const int RASTER_WIDTH = 1234;
+    const int RASTER_HEIGHT = 5566;
 
     MPI_Init(&argc, &argv);
 
@@ -34,7 +36,7 @@ int main(int argc, char *argv[])
 
         double geotransform[6] = {1.0, 0.1, 0, 40, 0, 0.1};
 
-        sptw::SPTW_ERROR err = sptw::create_tiled_raster(raster_fn, RASTER_SIZE, RASTER_SIZE, 1,
+        sptw::SPTW_ERROR err = sptw::create_tiled_raster(raster_fn, RASTER_WIDTH, RASTER_HEIGHT, 1,
                 GDT_Byte, geotransform, wkt, TILE_SIZE); 
 
         if (err != sptw::SP_None) {
@@ -69,22 +71,33 @@ int main(int argc, char *argv[])
     uint8_t tile_data[TILE_SIZE*TILE_SIZE];
     memset(tile_data, rank * 255 / process_count, TILE_SIZE*TILE_SIZE);
 
-    int tiles_per_rank = (RASTER_SIZE*RASTER_SIZE)/(TILE_SIZE*TILE_SIZE*process_count); 
+    int horizontal_tiles = std::ceil((double) RASTER_WIDTH / TILE_SIZE);
+    int vertical_tiles = std::ceil((double) RASTER_HEIGHT / TILE_SIZE);
+    int num_tiles = horizontal_tiles * vertical_tiles;
+
+    int tiles_per_rank = std::ceil((double)num_tiles / process_count); 
 
     std::cout << "Rank " << rank << " writing raster (" << tiles_per_rank << " tiles)" << std::endl;
-    
+   
     for(int i = 0; i < tiles_per_rank; i++)
     {
         int tile = i*process_count + rank;
 
-        int x = tile % (RASTER_SIZE / TILE_SIZE);
-        int y = tile / (RASTER_SIZE / TILE_SIZE);
+        int x = tile % horizontal_tiles;
+        int y = tile / horizontal_tiles;
 
-        sptw::write_area(raster, tile_data,
-                x*TILE_SIZE, y*TILE_SIZE,
-                x*TILE_SIZE+15, y*TILE_SIZE+15);
+        x *= TILE_SIZE;
+        y *= TILE_SIZE;
+
+        int w = std::min(TILE_SIZE, RASTER_WIDTH - x) - 1;
+        int h = std::min(TILE_SIZE, RASTER_HEIGHT - y) - 1;
+
+        //printf("Writing (%d,%d) -> (%d, %d)\n", x, y, x+w, y+h);
+        sptw::write_area(raster, tile_data, x, y, x+w, y+h);
     }
 
+    MPI_Barrier(MPI_COMM_WORLD);
+    
     sptw::close_raster(raster);
 
     MPI_Finalize();
